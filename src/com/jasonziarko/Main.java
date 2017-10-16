909e07aac847b495275400a24f671d9e3fe96e49
ziarkoconsulting <32423423+ziarkoconsulting@users.noreply.github.com>
2017-10-16 16:47:32 -0400

    Initial commit

---
+++ src/com/jasonziarko/Main.java
@@ -0,0 +1,15 @@
+package com.jasonziarko;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+	    String myString = "This is a string";
+        System.out.println("myString is equal to " + myString);
+
+        myString = myString + ", and this is more.";
+
+        System.out.println("myString is equal to " + myString);
+
+    }
+}
